2ccad094d27506e6dea7faa82ed7053bda3321e7
ll9 <llwierer@web.de>
2019-03-02 22:36:09 +0100

    AdoContext

---
+++ Sync2.0/Data/AdoContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sync2._0.Data
+{
+    public class AdoContext
+    {
+        public AdoContext()
+        {
+        }
+
+        public SqliteConnection GetConnection()
+        {
+            var connection = new SqliteConnection(ConfigurationManager.AppSettings["ConnectionString"]);
+            connection.Open();
+            return connection;
+        }
+    }
+}
