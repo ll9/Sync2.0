bc8292a99e98584d3810919f49d524505bff9dab
ll9 <llwierer@web.de>
2019-03-12 23:35:08 +0100

    AutoMapper Profile

---
+++ Sync2.0/AutoMapper/Profiles/MappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Sync2._0.DTOs;
+using Sync2._0.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sync2._0.AutoMapper.Profiles
+{
+    public class MappingProfile: Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<DynamicEntity, DynamicEntityDTO>()
+                .ForMember(dest => dest.ProjectId, opt => opt.MapFrom(src => src.ProjectTable.ProjectId))
+                .ReverseMap();
+
+            CreateMap<SchemaDefinition, SchemaDefinitionDTO>()
+                .ForMember(dest => dest.ProjectId, opt => opt.MapFrom(src => src.ProjectTable.ProjectId))
+                .ReverseMap();
+        }
+    }
+}
